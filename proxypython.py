35ccafd5ac58e720bf490d25c42478d020616cb7
Guilhermezkz <Guilhermezkz.gm@gmail.com>
2018-07-12 13:54:04 -0300

    Update proxypython.py

--- proxypython.py
+++ proxypython.py
@@ -15,7 +15,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:22'
-RESPONSE = 'HTTP/2 200 <font color="red">Python Proxy / Socks</font>\r\n\r\n'
+RESPONSE = 'HTTP/2 200 ok <font color="red">Python Proxy / Socks</font>\r\n\r\n'
 #RESPONSE = 'HTTP/2 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 
